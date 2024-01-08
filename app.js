93e5a91e7d5c6d529ed5658cd00e0b93cb98c061
Olakunle Hassan <kunlex_042003@yahoo.com>
2024-01-08 14:26:37 +0100

    adjustment on the container

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const acc = document.getElementsByClassName("accordion");
 let i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     let panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+    }
   });
 }
